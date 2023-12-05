b4236d8d4ab2f04d5a71b69530e9ee0a73a18b3e
Chaïmae <chaimaelyesri8@gmail.com>
2023-12-05 16:45:20 +0100

    Program base ready for develops

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const PokemonCard = ({ pokemon }) => {
+  const { id, name, image, types, height, weight, stats, evolvesTo } = pokemon;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div key={id} className="pokemon-card">
+      <img src={image} alt={name.en} />
+      <h3>{name.en}</h3>
+      <p>Type: {types.map(typeId => <span key={typeId}>{typeId} </span>)}</p>
+      <p>Height: {height} m</p>
+      <p>Weight: {weight} kg</p>
+      <p>Stats:</p>
+      <ul>
+        {Object.entries(stats).map(([stat, value]) => (
+          <li key={stat}>{stat}: {value}</li>
+        ))}
+      </ul>
+      <p>Evolves To:</p>
+      <ul>
+        {Object.entries(evolvesTo).map(([evolutionId, condition]) => (
+          <li key={evolutionId}>{evolutionId}: {condition}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+const App = () => {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://pokedex-api.3rgo.tech/api/pokemon');
+        setPokemonData(response.data.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Pokédex</h1>
+      <div className="pokemon-container">
+        {pokemonData.map(pokemon => (
+          <PokemonCard key={pokemon.id} pokemon={pokemon} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
