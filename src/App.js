5472f09747d2eaa8aa3b6547b9dc34b9a5b40ef6
Chaïmae <chaimaelyesri8@gmail.com>
2024-01-12 09:45:39 +0100

    changes to fix code

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import axios from "axios";
 import PokemonCard from "./components/PokemonCard";
 import Menu from "./components/Menu.jsx";
 import SearchBar from "./components/SearchBar";
-import PokemonSearchBar from './components/PokemonSearchBar';
 
 
 import "./App.css";
@@ -30,24 +29,14 @@ const App = () => {
     setPokemonNames(pokemonNamesList);
   }, [pokemonData]);
 
-  const handleSearch = () => {
-    // Call the onSearch function with the search filters and sorting options
-    onSearch({
-      generation: generationFilter,
-      type: typeFilter,
-      sort: sortOption,
-    });
-  };
 
   return (
     
     <div>
       <img src="Pokedex_logo.png" alt="Pokedex logo"></img>
-      <PokemonSearchBar onSearch={handleSearch} />
+
       <Menu />
-      {/* <div className="search-bar"> */}
       <SearchBar pokemonNames={pokemonNames} />
-      {/* </div> */}
       <div className="pokemon-container">
         {pokemonData.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
